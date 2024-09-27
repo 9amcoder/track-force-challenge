e7e0a2c12439de3c8d954a10f3a3b1d542ba515a
Steve Sultan <SteveSultan@outlook.com>
2024-09-26 21:52:27 -0400

    Refactor Dashboard component to fetch and display list of sites

--- src/app/(dashboard)/(routes)/dashboard/page.tsx
+++ src/app/(dashboard)/(routes)/dashboard/page.tsx
@@ -1,11 +1,34 @@
+"use client";
+import React, { useEffect } from "react";
+import SiteContainer from "@/components/dashboard/content/SiteContainer";
+import useSiteStore from "@/store/siteStore";
 
-
+/**
+ * Dashboard component that displays the list of sites.
+ * 
+ * @returns {JSX.Element} The rendered component.
+ */
 const Dashboard: React.FC = () => {
-    return ( 
-        <div>
-            <h1>Dashboard</h1>
-        </div>
-    );
-}
+  const { sites, siteLoading, error, fetchSites } = useSiteStore();
+
+  useEffect(() => {
+    fetchSites();
+  }, [fetchSites]);
+
+  if (siteLoading) {
+    return <div>Loading...</div>; // Replace with a spinner or loading component if available
+  }
+
+  if (error) {
+    return <div>Error: {error}</div>; // Provide more context if possible
+  }
+
+  return (
+    <div>
+      <h1>Dashboard</h1>
+      <SiteContainer sites={sites} />
+    </div>
+  );
+};
 
 export default Dashboard;
