8b37398e274eb19bf55890ec21b8273ee04674ed
Steve Sultan <SteveSultan@outlook.com>
2024-09-27 23:33:22 -0400

    Refactor dashboard page test to use correct page component

--- src/__test__/dashboard/page.test.tsx
+++ src/__test__/dashboard/page.test.tsx
@@ -1,10 +1,10 @@
 import { expect, test } from "vitest";
 import { render, screen } from "@testing-library/react";
-import Page from "../../app/(dashboard)/(routes)/dashboard/page";
+import Page from "../../app/(dashboard)/(routes)/scheduling/page";
 
 test("Dashboard Page renders without crashing", () => {
     render(<Page />);
     expect(
-        screen.getByRole("heading", { level: 1, name: "Dashboard" })
+        screen.getByRole("heading", { level: 1, name: "Sites" })
     ).toBeDefined();
 });
