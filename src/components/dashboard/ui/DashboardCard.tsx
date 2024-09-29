b953aa9f2ca52829fd2d69d728383352c1abaa9d
Steve Sultan <SteveSultan@outlook.com>
2024-09-28 21:20:46 -0400

    Refactor DashboardCard component to include site contact information and update styling

---
+++ src/components/dashboard/ui/DashboardCard.tsx
@@ -0,0 +1,61 @@
+import { Button } from "@/components/ui/button";
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import formatAddress from "@/helper/addressFormatter";
+import Site from "@/types/site";
+import { Separator } from "@radix-ui/react-dropdown-menu";
+import { Briefcase, ChevronLeft, Mail, Phone } from "lucide-react";
+import { useRouter } from "next/navigation";
+
+interface CardProps {
+  site: Site | null;
+  isMobile?: boolean;
+}
+
+const CardComponent: React.FC<CardProps> = ({ site }) => {
+  const router = useRouter();
+
+  return (
+    <Card className="relative flex flex-col items-center mt-5 md:mt-10 mx-5 md:mx-20">
+      <Button
+        size="icon"
+        variant="outline"
+        className="absolute top-1 left-1 md:top-2 md:left-2"
+        onClick={() => router.back()}
+      >
+        <ChevronLeft className="h-4 w-4" />
+      </Button>
+      <CardHeader className="text-center">
+        <CardTitle>
+          <div className="flex items-center justify-center">
+            {site?.contacts?.main?.firstName} {site?.contacts?.main?.lastName}
+          </div>
+        </CardTitle>
+      </CardHeader>
+      <CardContent className="flex flex-col items-center text-center">
+        <div className="w-full max-w-md">
+          <div className="flex items-center justify-center">
+            <Briefcase className="mr-2" />
+            {site?.contacts?.main?.jobTitle}
+          </div>
+          <Separator className="mt-5 w-full" />
+          <div className="flex items-center justify-center">
+            <Phone className="mr-2" />
+            {site?.contacts?.main?.phoneNumber}
+          </div>
+          <Separator className="mt-5 w-full" />
+          <div className="flex items-center justify-center">
+            <Mail className="mr-2" />
+            {site?.contacts?.main?.email}
+          </div>
+          <Separator className="mt-5 w-full" />
+          <div className="flex items-start justify-center text-sm md:text-lg ">
+            {/* <House size={30} className="mr-1" /> */}
+            {formatAddress(site?.address)}
+          </div>
+        </div>
+      </CardContent>
+    </Card>
+  );
+};
+
+export default CardComponent;
