a51c1babaee3c708daee5b0f6fac5a199dac6e5f
Steve Sultan <SteveSultan@outlook.com>
2024-09-28 21:20:22 -0400

    Refactor dashboard page to include media query hook and update site container styling

---
+++ src/app/(dashboard)/(routes)/scheduling/[id]/page.tsx
@@ -0,0 +1,33 @@
+"use client";
+import CardComponent from "@/components/dashboard/ui/DashboardCard";
+import SkeltonLoader from "@/components/dashboard/ui/SkeletonLoader";
+import useMediaQuery from "@/hooks/useMediaQuery";
+import useSiteStore from "@/store/siteStore";
+import { useParams } from "next/navigation";
+import { useEffect } from "react";
+
+const SiteDetailsPage: React.FC = () => {
+  const params = useParams<{ id: string }>();
+  const { site, siteLoading, error, fetchSite } = useSiteStore();
+  const { isMobile } = useMediaQuery();
+
+  useEffect(() => {
+    fetchSite(params.id);
+  }, [fetchSite, params.id]);
+
+  if (siteLoading) {
+    return <SkeltonLoader />;
+  }
+
+  if (error) {
+    return <div>Error: {error}</div>;
+  }
+
+  return (
+    <div>
+      <CardComponent site={site} isMobile={isMobile} />
+    </div>
+  );
+};
+
+export default SiteDetailsPage;
